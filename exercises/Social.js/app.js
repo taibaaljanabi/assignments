832759c3c2618e9423c2eb7c55580af030b2a6a2
Taiba Aljanabi <taibaaljanabi2001@gmail.com>
2019-07-10 23:45:47 -0600

    commit

---
+++ exercises/Social.js/app.js
@@ -0,0 +1,56 @@
+let singer={
+    name: 'Soso',
+    age: 55,
+    favoriteSongs: ['aaa','bbb', 'ccc', 'ddd'],
+    singingFriends: [
+        {
+            name: 'basha',
+            age: 66,
+            favoriteSongs: [
+                {
+                    songName: 'yala',
+                    difficulty: 10,
+                    old: true
+                },
+                {
+                    songName: 'Hawa',
+                    difficulty: 9,
+                    old: false
+                }
+            ]
+        },
+        {
+            name: 'Naal',
+            age: 99,
+            favoriteSongs:[
+                {
+                    songName: 'Hello',
+                    difficulty: 5,
+                    old: true
+                },
+                {
+                    songName: "yaHawa",
+                    difficulty: 10,
+                    old: false
+                }
+            ]
+        },
+        {
+            name: 'Nool',
+            age: 100,
+            favoriteSongs:[
+                {
+                    songName: 'Shako',
+                    difficulty: 9,
+                    old: false
+                },
+                {
+                    songName: "Mako",
+                    difficulty: 8,
+                    old: true
+                }
+            ]
+        }
+
+    ]
+}
