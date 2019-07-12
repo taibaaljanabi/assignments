28a78bc156ea1fcebc496fb814a7e55d16f4a978
Taiba Aljanabi <taibaaljanabi2001@gmail.com>
2019-07-12 10:59:38 -0600

    commit

--- exercises/Exercise-function/app.js
+++ exercises/Exercise-function/app.js
@@ -1,23 +1,50 @@
-// function addingFunction(numOne, numTwo){
-// return numOne + numTwo
-// }
+
 // const addingFunction = () => 1++
 // console.log(addingFunction("Bob", "Saget"))
 // Write a function that accepts two numbers as parameters, and returns the sum.
-
-// function adding(numOne, numTwo) {
+// function addingFunction(numOne, numTwo){
 //     return numOne + numTwo
+//     }
+//     console.log(addingFunction(2,3))
+
+
+// Write a function that accepts three numbers as parameters, and returns the largest of those numbers.
+// function comparing(num1,num2,num3){
+//    return (Math.max(num1,num2,num3)) 
+    
+    
+// }
+// console.log(comparing(1,2,3))
+    
+
+// Write a function that accepts one number as a parameter, and returns whether that number is even or odd. (Return the string "even" or "odd");
+
+// function evenOrOdd (num){
+//     if(num % 2 === 0){
+//       return ('even')
+//     }else{
+//        return ('odd')
+//     }
+// }
+// console.log(evenOrOdd(7))
+// Write a function that accepts a string as a parameter. If the length of the string is less than or equal to twenty characters long, return the string concatenated with itself (string + string). If the string is more than twenty characters long, return the first half of the string.
+
+// function stringLength (str){
+//     if(str.length <= 20){
+//         return(str.concate(str))
+//     }else{
+//         return (str.slice(0,10))
+//     }
+// }
+// console.log(stringLength('helloworldhowareyoudoing'))
+
+//OptionalChalenge
+// function number(n){
 
 // }
-// console.log(adding(1, 2))
-// Write a function that accepts three numbers as parameters, and returns the largest of those numbers.
-function comparing(){
-    Math.max( 1 , 2 , 3)
-    
-    
-}
-console.log(comparing())
-    
+
+
+
 
 
     
