d57cc0e81cd5a7fec3b3ad54523765ccc09e0bdb
Taiba Aljanabi <taibaaljanabi2001@gmail.com>
2019-07-15 11:25:55 -0600

    commit

--- exercises/Array Method Exercises/app.js
+++ exercises/Array Method Exercises/app.js
@@ -1,53 +1,57 @@
 
 
+const fruit = ["banana", "apple", "orange", "watermelon"]
+const vegetables = ["carrot", "tomato", "pepper", "lettuce"]
+//Remove the last item from the vegetable array.
+vegetables.pop()
 
-const vegetables = ["carrot", "tomato", "pepper", "lettuce"];
-const fruit = ["banana", "apple", "orange", "watermelon"];
-
-
-// Remove the last item from the vegetable array.
- vegetables.pop()
- 
 // Remove the first item from the fruit array.
- fruit.shift()
-// Find the index of "orange."
-var indexOfOrange = fruit.indexOf("orange")
-// Add that number to the end of the fruit array.
-var newArr4 = fruit.push(indexOfOrange)
-// Use the length property to find the length of the vegetable array.
-var vegL = vegetables.length
-// Add that number to the end of the vegetable array.
- vegetables.push(vegL)
-// Put the two arrays together into one array. Fruit first. Call the new Array "food".
-const food = fruit.concat(vegetables)
-// Remove 2 elements from your new array starting at index 4 with one method.
-var removing = food.splice(4,5,3 )
-// Reverse your array.
-var reversing = food.reverse()
-// Turn the array into a string and return it.
-var string = food.join(",")
+fruit.shift()
+
+//Find the index of "orange."
+let indexofOrange = fruit.indexOf('orange')
+
+//Add that number to the end of the fruit array.
+fruit.push(indexofOrange)
+
+//Use the length property to find the length of the vegetable array.
+let lengthOfVeg = vegetables.length
+
+//Add that number to the end of the vegetable array.
+vegetables.push(lengthOfVeg)
+
+//Put the two arrays together into one array. Fruit first. Call the new Array "food".
+
+let food = fruit.concat(vegetables)
+
+//Remove 2 elements from your new array starting at index 4 with one method.
+
+let removed = food.splice(4, 2)
+
+//Reverse your array.
+
+let reversedArray = food.reverse()
+
+
+
+//Turn the array into a string and return it.
+function arrayToString(arr){
+    return arr.toString()
+}
 
 
 
 
-// console.log("fruit :", fruit )
-// console.log("vegetables", vegetables)
-console.log(string)
 
 
 
 
+console.log("fruit: ", fruit)
+console.log("vegetables: ", vegetables)
+console.log(indexofOrange)
+console.log(lengthOfVeg)
+console.log(food)
+console.log('removed:', removed)
+console.log(reversedArray)
+console.log(arrayToString(food))
 
-
-
-
-
-// const vegetables = ["carrot", "tomato", "pepper", "lettuce"];
-
-// var editedArr = fruit.indexOf("orange")
-// var newArr = fruit.push(editedArr)
-// var arrLen = vegetables.length
-// var editedArr = fruit.indexOf("orange")var editedArr = fruit.indexOf("orange")
-// console.log(vegetables)
-// console.log(newVeg)
-// const food = []
