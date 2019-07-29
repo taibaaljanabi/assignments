1c23576a87e5e1be920f72a4d92bffcd1f0c226b
Taiba Aljanabi <taibaaljanabi2001@gmail.com>
2019-07-29 12:50:12 -0600

    commit

--- exercises/No Duplicates/app.js
+++ exercises/No Duplicates/app.js
@@ -1,19 +1,30 @@
-var name = "taiba amer  hussain hammadi al janabi"
-var newName = name.split("")
-var uniqueArr = [...new Set(newName)]
+// var name = "taiba amer  hussain hammadi al janabi"
+// var newName = name.split("")
+// var uniqueArr = [...new Set(newName)]
 // var selectLetter = Math.floor(Math.random() * uniqueArr.length)
 // var removeLetter = uniqueArr.splice(selectLetter, 1)
-function scramble (uniqueArr){
+// function scramble (uniqueArr){
     
-    while (uniqueArr.length > 0) {
+//     while (uniqueArr.length > 0) {
        
         
+//     }
+//    return uniqueArr 
+// }
+
+
+
+
+
+// console.log(scramble(uniqueArr))
+
+function find_unique_characters( string ){
+    let unique='';
+    for(let i=0; i<string.length; i++){
+        if(unique.indexOf(string[i]) === -1){
+            unique += string[i];
+        }
     }
-   return uniqueArr 
+    return unique;
 }
-
-
-
-
-
-console.log(scramble(uniqueArr))
+console.log(find_unique_characters('Taiba'));
